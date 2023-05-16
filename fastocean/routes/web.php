a04beb84f0c20ddbf44ccd140447742f4a1941a0
Raheel Saleem <raheelsaleem1000@gmail.com>
2023-05-16 16:15:16 +0500

    LoginController and View Created

--- fastocean/routes/web.php
+++ fastocean/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('signup', [LoginController::class, 'index'])->name('signup');
